718d6f666fc898e0f62d0d5e40c2813982c0111c
Raymond Michael Morales <michaelmorales53@gmail.com>
2020-01-24 01:20:35 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,26 +1,16 @@
-
+require 'pp'
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
 
+  programmer_hash = {}
+  
+  programmer_hash[:hopper] = {known_for: "COBOL",
+                              languages: ["COBOL", "FORTRAN"]}
+                                    
+  programmer_hash[:hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,6 +27,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +48,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][0]
+     
 end
 
 def adding_matz
@@ -77,10 +73,11 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
-     }
-
-    
+        },
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]}}
+     
 end
 
 def changing_alan
@@ -102,6 +99,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     programmer_hash
      
 end
 
@@ -121,5 +121,13 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+  help = programmer_hash[:dennis_ritchie][:languages]
+  
+  help.push("Assembly")
+  
+  programmer_hash[:dennis_ritchie][:languages] = help
+  
+  programmer_hash
 
 end
